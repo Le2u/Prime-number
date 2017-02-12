e776223197aa016bbb0daa4f9e2dd0a9c54316f5
Le2u <lijiacheng@lijiachengs-MacBook-Pro.local>
2017-02-12 14:32:11 +0800

    main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int x ;
+    
+    printf("请输入一个数以判断是非为素数:\n");
+    scanf("%d",&x);
+    
+    int i ;
+    int Prime = 1 ;
+    
+    for (i = 2 ; i < x ; i++ ) {
+        if ( x % i == 0 ) {
+            Prime = 0 ;
+            break ;
+        }
+    }
+    
+    if (Prime == 1) {
+        printf("是素数\n");
+    }else{
+        printf("不是素数\n");
+    }
+    return 0;
+}
